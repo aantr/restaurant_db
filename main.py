1c774702b6ccb1c97f7a9e607c213e1699029e0c
aantr <xgamestudio8@gmail.com>
2020-11-07 15:42:49 +0500

    #

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class App(QMainWindow):
         super().__init__()
 
         self.db_filename = 'restaurant_db.sqlite'
-        self.login_as_admin = True
+        self.login_as_admin = False
 
         self.stack_widgets = []
 
